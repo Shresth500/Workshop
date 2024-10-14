446fdf20bacebac8d788d085ffe79b85d8a36772
Prashanth Puranik <puranik@digdeeper.in>
2024-10-14 11:24:23 +0530

    primitive types, typeof, loosely typed

---
+++ demos/03-js/03-primitive-data-types.js
@@ -0,0 +1,25 @@
+let quantity = 3;
+console.log(typeof quantity); // "number" | "string" | "boolean"
+
+quantity = "Three";
+console.log(typeof quantity); // "number" | "string" | "boolean"
+
+const message1 = 'Single-quoted';
+const message2 = "Double-quoted";
+const message3 = `Backtick-quoted
+string`; // ES2015
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
+
+// string interpolation - supported inly backtick-quoted string
+// const message4 = `The number of items =` + quantity + ". Please confirm it is correct."
+const message4 = `The number of items = ${quantity}. Please confirm it is correct.`;
+console.log(message4);
+
+console.log(message4.length);
+console.log(message4[5]); // 6th character = "u"
+
+let isRaining = true;
+console.log(typeof isRaining); // "boolean"
