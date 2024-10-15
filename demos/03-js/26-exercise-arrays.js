5397239de903ef8274d9286e9da4c959393e93ee
Prashanth Puranik <puranik@digdeeper.in>
2024-10-15 12:33:52 +0530

    exercise on arrays

---
+++ demos/03-js/26-exercise-arrays.js
@@ -0,0 +1,22 @@
+const phones = [
+    {
+        name: 'Samsung Galaxy S10+ Plus', price: 620, type: 'refurbished',
+        manufacturer: 'Samsung'
+    },
+    {
+        name: 'Apple iPhone 7 Plus', price: 450, type: 'refurbished',
+        manufacturer: 'Apple'
+    },
+    { name: 'One Plus 6', price: 430, type: 'new', manufacturer: 'OnePlus' },
+    {
+        name: 'Apple iPhone Xs', price: 910, type: 'new', manufacturer: 'Apple'
+    },
+    {
+        name: 'One Plus 7', price: 430, type: 'refurbished', manufacturer:
+            'OnePlus'
+    },
+    {
+        name: 'Apple iPhone 8 Plus', price: 610, type: 'new', manufacturer:
+            'Apple'
+    },
+];
