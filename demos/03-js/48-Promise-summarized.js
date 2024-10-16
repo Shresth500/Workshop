92770d7b2d45d3290e45aa9c014d323793845f8e
Prashanth Puranik <puranik@digdeeper.in>
2024-10-16 13:28:30 +0530

    chaining promises

--- demos/03-js/48-Promise-summarized.js
+++ demos/03-js/48-Promise-summarized.js
@@ -1,7 +1,7 @@
 function sumAsync(x, y) {
     return new Promise(
         (resolve, reject) => { // f
-            console.log('f called');
+            // console.log('f called');
 
             setTimeout(
                 () => {
@@ -18,11 +18,32 @@ const p = sumAsync(12, 13)
 
 // Hey Promise object! When you get the result, give me the result
 // g will be called when the promise gets the result
-p.then(
-    (result) => { // g
-        console.log('result = ', result);
-    }
-)
+p
+    .then(
+        (result1) => { // g
+            console.log('result1 = ', result1);
+
+            // sumAsync(result1, 14).then(
+            //     (result2) => {
+            //         console.log('result2 =', result2);
+            //     }
+            // )
+
+            return sumAsync(result1, 14); // q
+        }
+    ) // r (when q completes with the result, r will also complete with the same result)
+    .then(
+        result2 => {
+            console.log('result2 =', result2);
+
+            return sumAsync(result2, 15); // s
+        }
+    ) // t
+    .then(
+        result3 => {
+            console.log('result3 =', result3);
+        }
+    )
 
 // class Promise {
 //     constructor( setupFunction ) {
