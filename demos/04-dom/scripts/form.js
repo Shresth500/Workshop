a37453a6328128bb0f6a3821120a6953e5acb9d1
Prashanth Puranik <puranik@digdeeper.in>
2024-10-17 10:15:53 +0530

    input, blur event -> form validation

---
+++ demos/04-dom/scripts/form.js
@@ -0,0 +1,41 @@
+const phoneEl = document.getElementById('phone');
+const phoneErrorEl = document.getElementById('phone-error');
+let phoneElTouched = false;
+
+// regular expressions for pattern matching in strings
+// const mobilePhonePat = /[6-9][0-9]{9}/;
+// ^ -> match from the first character
+// $ -> match till the last character
+const mobilePhonePat = /^[6-9][0-9]{9}$/;
+
+console.log(mobilePhonePat.test("9123456780")); // true
+console.log(mobilePhonePat.test("5123456780")); // false
+console.log(mobilePhonePat.test("912345678")); // false
+// console.log(mobilePhonePat.test("912345678012")); // true if substring match is allowed
+console.log(mobilePhonePat.test("912345678012")); // false
+
+function validatePhone() {
+    console.log(this); // same as phoneEl
+
+    // input DOM node - <input />, <select></select>, <textarea></textarea>, have a property called value
+    console.log(this.value);
+
+    const value = this.value;
+
+    if (phoneElTouched) {
+        if (mobilePhonePat.test(value)) {
+            this.nextElementSibling.innerText = '';
+        } else {
+            this.nextElementSibling.innerText = 'Phone number is not valid. It should have 10 digits.'
+        }
+    }
+}
+
+phoneEl.addEventListener('input', validatePhone);
+// phoneEl.addEventListener('blur', validatePhone );
+
+phoneEl.addEventListener('blur', function () {
+    phoneElTouched = true;
+
+    validatePhone.call(phoneEl);
+})
