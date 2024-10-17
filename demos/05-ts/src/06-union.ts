f33ea0307df2845421bbc74168b8d49c40a2e4e5
Prashanth Puranik <puranik@digdeeper.in>
2024-10-17 12:52:25 +0530

    union, type alias

---
+++ demos/05-ts/src/06-union.ts
@@ -0,0 +1,13 @@
+let chequeAmount: number | string = 10000;
+
+chequeAmount = "Twenty thousand";
+// chequeAmount = true;
+// chequeAmount = {
+//     value: 30000;
+// };
+
+// NOTE: Union type can be used with both primitives and non-primitives
+
+export {
+    chequeAmount
+}
---
+++ demos/05-ts/src/07-type-alias.ts
@@ -0,0 +1,9 @@
+import { chequeAmount } from "./06-union";
+
+type NS = number | string;
+
+// let distance : number | string = "Thousand Two Hundred";
+let distance: NS = "Thousand Two Hundred";
+distance = 1200;
+
+// NOTE: Type alias works with primitives and non-primitives
