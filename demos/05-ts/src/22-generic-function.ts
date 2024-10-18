70cd31d7ef345d386ae05bfc4c3dbe90d1cec7e3
Prashanth Puranik <puranik@digdeeper.in>
2024-10-18 15:02:47 +0530

    generic functions

--- demos/05-ts/src/22-generic-function.ts
+++ demos/05-ts/src/22-generic-function.ts
@@ -1,10 +1,41 @@
-type MapperFunction<InputItem, OutputItem> = (item: InputItem) => OutputItem;
+// type MapperFunction<InputItem, OutputItem> = (item: InputItem) => OutputItem;
 
-function map<InputItem, OutputItem>(
-    arr: InputItem[],
-    mapperFunction: MapperFunction<InputItem, OutputItem>
-) {
-    const result: OutputItem[] = [];
+// function map<InputItem, OutputItem>(
+//     arr: InputItem[],
+//     mapperFunction: MapperFunction<InputItem, OutputItem>
+// ) {
+//     const result: OutputItem[] = [];
+
+//     for (let i = 0; i < arr.length; ++i) {
+//         result.push(mapperFunction(arr[i]));
+//     }
+
+//     return result;
+// }
+
+// const mappedDays = map([1, 2, 3, 4, 0, 4, 6, 5], (dayNumber: number) => {
+//     const days = [
+//         "Sunday",
+//         "Monday",
+//         "Tuesday",
+//         "Wednesday",
+//         "Thursday",
+//         "Friday",
+//         "Saturday",
+//     ];
+
+//     return days[dayNumber];
+// });
+
+// const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item: number) => {
+//     return item * item;
+// });
+
+// --------
+type MapperFunction<T, S> = (x: T) => S;
+
+function map<T, S>(arr: T[], mapperFunction: MapperFunction<T, S>) {
+    const result = [];
 
     for (let i = 0; i < arr.length; ++i) {
         result.push(mapperFunction(arr[i]));
@@ -13,20 +44,6 @@ function map<InputItem, OutputItem>(
     return result;
 }
 
-const mappedDays = map([1, 2, 3, 4, 0, 4, 6, 5], (dayNumber: number) => {
-    const days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
-
-    return days[dayNumber];
-});
-
-const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item: number) => {
+const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item) => {
     return item * item;
 });
