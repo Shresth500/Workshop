e78a4b1adf181f263c85ff748b6d9a0bc59b66a3
Prashanth Puranik <puranik@digdeeper.in>
2024-10-18 15:16:41 +0530

    inference of types when function is called

--- demos/05-ts/src/22-generic-function.ts
+++ demos/05-ts/src/22-generic-function.ts
@@ -1,41 +1,10 @@
-// type MapperFunction<InputItem, OutputItem> = (item: InputItem) => OutputItem;
+type MapperFunction<InputItem, OutputItem> = (item: InputItem) => OutputItem;
 
-// function map<InputItem, OutputItem>(
-//     arr: InputItem[],
-//     mapperFunction: MapperFunction<InputItem, OutputItem>
-// ) {
-//     const result: OutputItem[] = [];
-
-//     for (let i = 0; i < arr.length; ++i) {
-//         result.push(mapperFunction(arr[i]));
-//     }
-
-//     return result;
-// }
-
-// const mappedDays = map([1, 2, 3, 4, 0, 4, 6, 5], (dayNumber: number) => {
-//     const days = [
-//         "Sunday",
-//         "Monday",
-//         "Tuesday",
-//         "Wednesday",
-//         "Thursday",
-//         "Friday",
-//         "Saturday",
-//     ];
-
-//     return days[dayNumber];
-// });
-
-// const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item: number) => {
-//     return item * item;
-// });
-
-// --------
-type MapperFunction<T, S> = (x: T) => S;
-
-function map<T, S>(arr: T[], mapperFunction: MapperFunction<T, S>) {
-    const result = [];
+function map<InputItem, OutputItem>(
+    arr: InputItem[],
+    mapperFunction: MapperFunction<InputItem, OutputItem>
+) {
+    const result: OutputItem[] = [];
 
     for (let i = 0; i < arr.length; ++i) {
         result.push(mapperFunction(arr[i]));
@@ -44,6 +13,40 @@ function map<T, S>(arr: T[], mapperFunction: MapperFunction<T, S>) {
     return result;
 }
 
-const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item) => {
+// TypeScript "infers" the types T and S when the function is called byb comparing the passed values to the generic function signature
+const mappedDays = map([1, 2, 3, 4, 0, 4, 6, 5], (dayNumber: number) => {
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    return days[dayNumber];
+});
+
+const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item: number) => {
     return item * item;
 });
+
+// --------
+// type MapperFunction<T, S> = (x: T) => S;
+
+// function map<T, S>(arr: T[], mapperFunction: MapperFunction<T, S>) {
+//     const result = [] as S[];
+
+//     for (let i = 0; i < arr.length; ++i) {
+//         result.push(mapperFunction(arr[i]));
+//     }
+
+//     return result;
+// }
+
+// const mappedResult = map([1, 2, 3, 4, 0, 4, 6, 5], (item) => {
+//     return item * item;
+// });
+
+// EXERCISE: Create a generic version of contains() function that we worked on earlier
